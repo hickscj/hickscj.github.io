56432cee54e78f47831c46790eeffbfff11ae063
Chad Hicks <chadjhicks@gmail.com>
2020-12-18 10:56:00 -0500

    work in progress BarChart component - eslintcache ignore

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,20 +1,34 @@
-import React from 'react';
-//import * as d3 from 'd3';
+import React, { Component } from 'react';
+import * as d3 from 'd3';
 
-const BarChart = () => {
-    return (
-        <svg
-            style={{
-                height: 400,
-                width: "100%"
-            }}
-        >
-            <g className="plot-area" />
-            <g className="x-axis" />
-            <g className="y-axis" />
-        </svg>
-    )
-};
+class BarChart extends Component {
+
+    drawChart() {
+        const data = [12, 5, 6, 6, 9, 10];
+
+        const svg = d3.select('#bar-chart').append('svg')
+            .attr('width', 700)
+            .attr('height', 200);
+
+        svg.selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', 65)
+            .attr('height', 101)
+            .attr('fill', 'green');
+    }
+
+    componentDidMount() {
+        this.drawChart();
+    }
+
+    render() {
+        return <div id='bar-chart'></div>;
+    }
+}
 
 export default BarChart;
 
