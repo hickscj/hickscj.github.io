ddfeece12942f5bd99a7c775dc1063effac63de5
Chad Hicks <chadjhicks@gmail.com>
2021-02-02 23:13:28 -0500

    working skills charts

--- src/Skills.js
+++ src/Skills.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BarChart, Bar, XAxis, Tooltip } from 'recharts';
+const data = [
+    {name: 'JavaScript', years: 8, proficiency: 10},
+    {name: 'PHP', years: 10, proficiency: 10},
+    {name: 'CSS', years: 10, proficiency: 12},
+    {name: 'SQL', years: 10, proficiency: 12}
+];
 
 const Skills = () => {
     return (
-        <div>SKILLS</div>
+        <div>
+            <BarChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{ top: 5, right: 20, left: 45, bottom: 5 }}
+            >
+            <XAxis dataKey="name" />
+            <Tooltip />
+            {/* <CartesianGrid stroke="#ccc" /> */}
+            <Bar type="monotone" dataKey="years" fill="#ff7300" yAxisId={0} />
+            <Bar type="monotone" dataKey="proficiency" fill="#197278" yAxisId={0} />
+            </BarChart>
+        </div>
     );
 };
 
