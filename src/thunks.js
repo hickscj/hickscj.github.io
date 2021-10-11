5d7ea85fc0843a37b72d2c66f55c3af85f488814
Chad Hicks <chadjhicks@gmail.com>
2021-10-11 14:03:42 -0400

    Remove api config

--- src/thunks.js
+++ src/thunks.js
@@ -2,24 +2,25 @@ import {
     loadJobsInProgress,
     loadJobsSuccess
 } from "./actions";
-import db from './firebase-config';
+// import db from './firebase-config';
 // import { collection, getDocs } from "firebase/firebase-firestore";
 
 export const loadJobs = () => async (dispatch) => {
     try {
         dispatch(loadJobsInProgress());
-        // const response = await fetch('jobject.json');
-        // const jobs = await response.json();
+        const response = await fetch('jobject.json');
+        const jobs = await response.json();
         // const response = await collection(db, 'jobs');
         // const jobs = await getDocs(response);
 
-        db.collection('jobs').get().then(snap => {
-            const jobs = [];
-            snap.forEach(job => jobs.push(job.data()));
-            return jobs;
-        }).then(jobs => {
-            dispatch(loadJobsSuccess(jobs));
-        });
+        // db.collection('jobs').get().then(snap => {
+        //     const jobs = [];
+        //     snap.forEach(job => jobs.push(job.data()));
+        //     return jobs;
+        // }).then(jobs => {
+        //     dispatch(loadJobsSuccess(jobs));
+        // });
+        dispatch(loadJobsSuccess(jobs));
     } catch (e) {
         alert(`Error loading from firebase... ${e}`);
     }
