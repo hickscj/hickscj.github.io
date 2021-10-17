6b305cc21b0811efaa7492d69fb11a8b89ccd735
Chad Hicks <chadjhicks@gmail.com>
2021-10-17 11:01:46 -0400

    Clean up and organize files

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { persistStore } from "redux-persist";
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
-import { configureStore } from "./store";
+import { configureStore } from "./store/store";
 import App from "./App";
-import './index.scss';
+import './styles/index.scss';
 
 const store = configureStore();
 const persistor = persistStore(store);
