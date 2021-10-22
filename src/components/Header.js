f7ff5d147d62e4fb0c50001dff83a7cb6f6e0301
Chad Hicks <chadjhicks@gmail.com>
2021-10-21 21:44:16 -0400

    update header spacing and resume links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 export const Header = (props) => {
     return (
-        <header>
+        <header className="pt-5">
             <img className="face ring-4 ring-yellow-500 ring-opacity-50" src="../img/chad_hicks.jpg" alt="" />
             <h2 className="select-none">Chad Hicks</h2>
             <ul className="p-4">
                 <li><a href="/" onClick={ props.toAbout }>About Me</a></li>
                 <li><a href="https://github.com/hickscj">GitHub</a></li>
-                <li><a href="../resources/chad.hicks_Oct2021.pdf">Resume</a></li>
+                <li><a href="../resources/chad.hicks_Oct2021_resume.pdf">Resume (pdf)</a></li>
+                <li><a href="../resources/chad.hicks_Oct2021_resume.docx">Resume (docx)</a></li>
             </ul>
             <div className="pt-1">
                 <img align="right"
