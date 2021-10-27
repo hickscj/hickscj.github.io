655b2ea57a9d84ef18ec469e05b8af984802c2e4
Chad Hicks <chadjhicks@gmail.com>
2021-10-27 00:44:32 -0400

    build files

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export const Header = (props) => {
     return (
         <header className="pt-5">
             <img className="face ring-4 ring-yellow-500 ring-opacity-50" src="../img/chad_hicks.jpg" alt="" />
-            <h2 className="select-none">Chad Hicks</h2>
+            <h2 className="select-none pl-2">Chad Hicks</h2>
             <ul className="p-4">
                 <li><a href="/" onClick={ props.toAbout }>About Me</a></li>
                 <li><a href="https://github.com/hickscj">GitHub</a></li>
