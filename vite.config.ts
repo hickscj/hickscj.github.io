1c9edfe6f8c4f4d50568f32bd16d326862a145fc
Chad <hickscj@users.noreply.github.com>
2024-03-08 18:24:31 +0000

    update layout and build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    base: '',
+    build: {
+        outDir: 'build'
+    },
     plugins: [react(), viteTsconfigPaths()],
     server: {    
         open: true,
