d75ac1d28b2d1b3b7c8b46f90cc8e872605dea27
Chad Hicks <chadjhicks@gmail.com>
2025-04-13 07:36:45 -0500

    adding awards

--- src/components/Accomplishments.tsx
+++ src/components/Accomplishments.tsx
@@ -2,10 +2,41 @@
 
 
 export const Accomplishments = () => {
+    const awards = [
+        {
+            title: "",
+            description: "",
+            imageUrl: "../resources/Copper_award_from_James.png"
+        },
+        {
+            title: "",
+            description: "",
+            imageUrl: "../resources/ecard-from-Rian.png"
+        },
+        {
+            title: "",
+            description: "",
+            imageUrl: "../resources/disruptive-tech-award-blurb.png"
+        },
+        {
+            title: "",
+            description: "",
+            imageUrl: "../resources/workday-feedback.png"
+        },
+    ];
+
     return (
         <div>
-            <h3>Accolades, Articles and Awards</h3>
-            <p>Ruby Award</p>
+            <h3>Awards</h3>
+            <ul>
+                {awards.map((award, index) => (
+                    <li key={index}>
+                        <h4>{award.title}</h4>
+                        <p>{award.description}</p>
+                        <img src={award.imageUrl} alt={award.title} />
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
