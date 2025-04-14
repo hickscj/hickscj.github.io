9649fda4a60da6e7e262e9feac29f32fc930f3bf
Chad Hicks <chadjhicks@gmail.com>
2025-04-13 22:42:27 -0500

    updates to accomplishments page

--- src/components/Accomplishments.tsx
+++ src/components/Accomplishments.tsx
@@ -2,41 +2,46 @@
 
 
 export const Accomplishments = () => {
-    const awards = [
-        {
-            title: "",
-            description: "",
-            imageUrl: "../resources/Copper_award_from_James.png"
-        },
-        {
-            title: "",
-            description: "",
-            imageUrl: "../resources/ecard-from-Rian.png"
-        },
-        {
-            title: "",
-            description: "",
-            imageUrl: "../resources/disruptive-tech-award-blurb.png"
-        },
-        {
-            title: "",
-            description: "",
-            imageUrl: "../resources/workday-feedback.png"
-        },
-    ];
+  const awards = [
+    {
+      title: "",
+      description: "",
+      imageUrl: "../resources/copper-award-1.png"
+    },
+    {
+      title: "",
+      description: "",
+      imageUrl: "../resources/copper-award-2.png"
+    },
+    {
+      title: "",
+      description: "",
+      imageUrl: "../resources/ecard-from-Rian.png"
+    },
+    {
+      title: "",
+      description: "",
+      imageUrl: "../resources/disruptive-tech-award-blurb-sm.png"
+    },
+    {
+      title: "",
+      description: "",
+      imageUrl: "../resources/workday-feedback-sm.png"
+    },
+  ];
 
-    return (
-        <div>
-            <h3>Awards</h3>
-            <ul>
-                {awards.map((award, index) => (
-                    <li key={index}>
-                        <h4>{award.title}</h4>
-                        <p>{award.description}</p>
-                        <img src={award.imageUrl} alt={award.title} />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <h3 className="text-2xl font-bold pb-5">Awards and Feedback</h3>
+      <ul>
+        {awards.map((award, index) => (
+          <li key={index}>
+            <h4>{award.title}</h4>
+            <p>{award.description}</p>
+            <img src={award.imageUrl} alt={award.title} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
